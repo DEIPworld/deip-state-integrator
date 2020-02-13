6f28122c49e3a0b68f840ce69920eabc4bcbde8c
egortsaryk9 <egortsaryk@gmail.com>
2020-02-13 20:53:03 +0300

    Docker configuration

--- docker-build.sh
+++ docker-build.sh
@@ -3,12 +3,17 @@ set -e
 
 TAG=$(git log -1 --pretty=%h)
 LATEST="latest"
+ORG="deipworld"
+IMAGE_PREFIX="$1"
 
-echo "Building deipworld/ip-protection-state-integrator image..."
-export IMAGE_NAME="deipworld/ip-protection-state-integrator:$TAG"
-export LATEST_IMAGE_NAME="deipworld/ip-protection-state-integrator:$LATEST"
+echo "Building $ORG/$IMAGE_PREFIX-state-integrator image..."
+export IMAGE_NAME="$ORG/$IMAGE_PREFIX-state-integrator:$TAG"
+export LATEST_IMAGE_NAME="$ORG/$IMAGE_PREFIX-state-integrator:$LATEST"
+
 export NODE_ENV="production"
 docker build -t=${IMAGE_NAME} .
 docker tag ${IMAGE_NAME} ${LATEST_IMAGE_NAME}
 docker push ${IMAGE_NAME}
 docker push ${LATEST_IMAGE_NAME}
+docker rmi ${IMAGE_NAME}
+docker rmi ${LATEST_IMAGE_NAME}
